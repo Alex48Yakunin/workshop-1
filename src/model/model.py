d82f2f74b90ab81314180d6fb647f64949029d76
Alex48Yakunin <sanchezz.jakunin@rambler.ru>
2024-02-25 18:48:54 +0300

    Update model.py

--- src/model/model.py
+++ src/model/model.py
@@ -39,27 +39,33 @@ def get_text(tokenizer, model, text):
     """
 
     if text == "":
-        return "Введите текст в окно выше"      # Проверка на пустую строку. Иначе модель выдаст неадекватный результат.
+        return "Введите текст в окно выше"  # Проверка на пустую строку
 
     model_inputs = tokenizer(
-        [text],                                 # Тест для токенизации (разделения на части) и векторизации.
-        max_length=1024,                        # Максимальное количество токенов в векторе. Максимум модели 1024.
-        padding=False,                          # Если токенов в векторе меньше чем max_length, то дополнить длину пустыми токенами.
-        truncation=True,                        # Если токенов в векторе больше чем max_length, то включить усечение.
-        return_tensors="pt"                     # Использовать библиотеку PyTorch для векторизации.
+        [text],  # Тест для токенизации (разделения на части) и векторизации
+        max_length=1024,  # Максимум модели 1024
+        padding=False,  # токенов  меньше чем max_length, дополнить пустыми
+        truncation=True,  # токенов больше чем max_length, включить усечение
+        return_tensors="pt",  # библиотека PyTorch для векторизации
     )
 
-    input_ids = model_inputs["input_ids"]       # Вектор входящего текста.
-    input_length = input_ids.shape[1]           # Сюда попадает фактическая длина вектора.
-    _min_length = int(input_length * 0.1)       # Минимальное количество токенов в результирующем векторе. Установлено как 10% от input_length.
-    min_length = _min_length if _min_length <= 200 else 200             # Должно быть меньше или равно 200.
+    input_ids = model_inputs["input_ids"]  # Вектор входящего текста
+    input_length = input_ids.shape[1]  # фактическая длина вектора
+    _min_length = int(
+        input_length * 0.1
+    )  # Минимальное количество токенов установлено как 10% от input_length
+    min_length = (
+        _min_length if _min_length <= 200 else 200
+    )  # Должно быть меньше или равно 200.
 
     model_outputs = model.generate(
-        input_ids=input_ids,                    # Векторизированный текст передаётся модели.
+        input_ids=input_ids,  # Векторизированный текст передаётся модели
         min_length=min_length,
-        no_repeat_ngram_size=4                  # Последовательности из N значений данного размера могут встречаться 1 раз.
+        no_repeat_ngram_size=4,  # Последовательности могут встречаться 1 раз
     )
-    output_ids = model_outputs[0]               # Вектор исходящего текcта.
+    output_ids = model_outputs[0]  # Вектор исходящего текcта.
 
-    summary = tokenizer.decode(output_ids, skip_special_tokens=True)    # Декодирование - превращение вектора в текст.
+    summary = tokenizer.decode(
+        output_ids, skip_special_tokens=True
+    )  # Декодирование - превращение вектора в текст.
     return summary
